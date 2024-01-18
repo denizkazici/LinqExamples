47ef7c29597161a59cdcf34af9a2d0e199737eb7
PC <PC@DESKTOP-FTBVLSJ>
2024-01-18 17:44:29 +0300

    Add project files.

---
+++ LinqExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System;
+
+List<int> NumberList = new List<int>(20);
+Random random = new Random();
+for (int i = 0; i < 20; i++)
+{
+
+    NumberList.Add(random.Next(1, 101));
+}
+
+var whereSubList = NumberList.Where(x => x > 8).ToList();
+
+var maxNum = NumberList.Max(x => x);
+var minNum = NumberList.Min(x => x);
+var avgNum = NumberList.Average(x => x);
+
+var selectSubList = NumberList.Select(x => x / 2);
+
+var orderList = NumberList.OrderBy(x => x).ToList();
+
+var SubList = NumberList.Take(5).ToList();
+
+var AfterList = NumberList.Skip(5).ToList();
+
+var checkEvenNum = NumberList.Any(x => x % 2 == 0);
+
+var CheckAllEven = NumberList.All(x => x % 2 == 0);
+
+
+foreach (int i in SubList)
+{
+    Console.WriteLine(i);
+}
